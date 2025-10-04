a43d7172ad1a8caee9d4fcd4f49182f7337ca721
supremex04 <bhandarisupreme7@gmail.com>
2025-10-04 16:25:19 +0545

    websocket listener

--- src/main/java/com/example/realtimechatapp/listner/WebSocketListener.java
+++ src/main/java/com/example/realtimechatapp/listner/WebSocketListener.java
@@ -1,4 +1,35 @@
 package com.example.realtimechatapp.listner;
 
+import com.example.realtimechatapp.service.UserService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.logging.Logger;
+
 public class WebSocketListener {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketListener.class);
+
+    @EventListener
+    public void handleWebsocketConnectListener(SessionConnectedEvent event){
+        logger.info("Connected to websocket");
+
+    }
+
+    public void handleWebsocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+    }
+
+
+
+
 }
