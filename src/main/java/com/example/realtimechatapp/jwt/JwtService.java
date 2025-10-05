a76f9289ec270c0ee8c7f29596810fa8e1b81246
supremex04 <bhandarisupreme7@gmail.com>
2025-10-05 18:08:09 +0545

    jwt service

--- src/main/java/com/example/realtimechatapp/jwt/JwtService.java
+++ src/main/java/com/example/realtimechatapp/jwt/JwtService.java
@@ -1,7 +1,93 @@
 package com.example.realtimechatapp.jwt;
 
+import com.example.realtimechatapp.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration;
+
+    // extract the User ID from JWT token
+
+    public Long extractUserId(String jwtToken){
+        String userIdStr = extractClaim(jwtToken, claims -> claims.get("userId", String.class));
+
+        return userIdStr != null ? Long.parseLong(userIdStr): null;
+
+    }
+
+    private <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver){
+        final Claims claims =  extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+
+    }
+
+    public Claims extractAllClaims(String jwtToken){
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
+    }
+
+    public SecretKey getSignInKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(User user){
+        return generateToken(new HashMap<>(), user);
+
+
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, User user){
+        Map<String, Object> claims = new HashMap<>(extraClaims);
+        claims.put("userId", user.getId());
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+
+    }
+
+    public boolean isTokenValid(String jwtToken, User user){
+        final Long userIdFromToken = extractUserId(jwtToken);
+        final Long userId = user.getId();
+        return (userIdFromToken != null && userIdFromToken.equals(userId) && !isTokenExpired(jwtToken));
+
+    }
+
+    private boolean isTokenExpired(String jwtToken){
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    private Date extractExpiration(String jwtToken){
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+
+
+
+
+
 }
